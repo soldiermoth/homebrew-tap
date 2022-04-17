9135dac84c1cc9c823b53bd54d01ca29526b1c10
goreleaserbot <goreleaser@carlosbecker.com>
2022-04-17 09:54:51 -0700

    Brew formula update for hlsq version v0.1.3

--- Formula/hlsq.rb
+++ Formula/hlsq.rb
@@ -5,12 +5,20 @@
 class Hlsq < Formula
   desc "Add some color to your HLS manifests"
   homepage "https://github.com/soldiermoth/hlsq"
-  version "0.1.2"
+  version "0.1.3"
 
   on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/soldiermoth/hlsq/releases/download/v0.1.3/hlsq_0.1.3_Darwin_arm64.tar.gz"
+      sha256 "2969c4c39fa0a49aa35a9c253f3d5f4ca475d70d727716fac3c132a237fd2434"
+
+      def install
+        bin.install "hlsq"
+      end
+    end
     if Hardware::CPU.intel?
-      url "https://github.com/soldiermoth/hlsq/releases/download/v0.1.2/hlsq_0.1.2_Darwin_x86_64.tar.gz"
-      sha256 "71db76065c3a0899aaac6f6c8de2e489c6158f466a1dadba5b32db1b45e4eb7f"
+      url "https://github.com/soldiermoth/hlsq/releases/download/v0.1.3/hlsq_0.1.3_Darwin_x86_64.tar.gz"
+      sha256 "25fb5b998318f6eff789c19f07f0ff368c6fee42f35c7029096d2eb8cb0e6081"
 
       def install
         bin.install "hlsq"
@@ -20,16 +28,24 @@ class Hlsq < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/soldiermoth/hlsq/releases/download/v0.1.2/hlsq_0.1.2_Linux_x86_64.tar.gz"
-      sha256 "27b790a677f440281da83ffade0c6c5fa5c69216b1db2dd9f00d64f5e302bb69"
+      url "https://github.com/soldiermoth/hlsq/releases/download/v0.1.3/hlsq_0.1.3_Linux_x86_64.tar.gz"
+      sha256 "1b0ec4dac1fe9ce17f13ed69858f599ce749d20dc0f6f4b2cb751a37d1cb6754"
+
+      def install
+        bin.install "hlsq"
+      end
+    end
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/soldiermoth/hlsq/releases/download/v0.1.3/hlsq_0.1.3_Linux_armv6.tar.gz"
+      sha256 "523d07c310afc6d45bfa575a2ed36c05740be783e9b8d7723abe7d7ff833ebde"
 
       def install
         bin.install "hlsq"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/soldiermoth/hlsq/releases/download/v0.1.2/hlsq_0.1.2_Linux_arm64.tar.gz"
-      sha256 "13b9ebd3b1aa8fa46157222ddb0eb9c3fdc65c7adec69257c52f61eeca7b9e4b"
+      url "https://github.com/soldiermoth/hlsq/releases/download/v0.1.3/hlsq_0.1.3_Linux_arm64.tar.gz"
+      sha256 "953ddcc1157d73793ab96d76be0d50c1886c0c8a0d929021f417752db032bdd1"
 
       def install
         bin.install "hlsq"
